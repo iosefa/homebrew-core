880a27cb5d6cd75130de63d5d48d2ff2cacaae2a
ilovezfs <ilovezfs@icloud.com>
2016-06-13 05:30:49 -0700

    perl-build 1.13
    Also, add a test.

--- Formula/perl-build.rb
+++ Formula/perl-build.rb
@@ -1,13 +1,18 @@
 class PerlBuild < Formula
   desc "Perl builder"
   homepage "https://github.com/tokuhirom/Perl-Build"
-  url "https://github.com/tokuhirom/Perl-Build/archive/1.12.tar.gz"
-  sha256 "2a3c33b7ea63c511db20778ebcedc35a0cc53fb780fc61b23d48ef3cc216bfc8"
+  url "https://github.com/tokuhirom/Perl-Build/archive/1.13.tar.gz"
+  sha256 "921880b901ce8322577ae8004cb3214e1223f5ecef0553d02419e7aa265c76b8"
   head "https://github.com/tokuhirom/perl-build.git"
 
   bottle :unneeded
 
   def install
+    inreplace "perl-build", "our $VERSION = '1.12", "our $VERSION = '#{version}"
     bin.install "perl-build", "bin/plenv-install", "bin/plenv-uninstall"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/perl-build --version")
   end
 end
