2e03cc5e2d71e9dd97d18671ff69c753f3abbe81
ilovezfs <ilovezfs@icloud.com>
2016-07-02 10:27:32 -0700

    slurm 1

--- Formula/slurm.rb
+++ Formula/slurm.rb
@@ -1,8 +1,8 @@
 class Slurm < Formula
   desc "Yet another network load monitor"
   homepage "https://github.com/mattthias/slurm"
-  url "https://github.com/mattthias/slurm/archive/upstream/0.4.2.tar.gz"
-  sha256 "8a28e11650928d87a907f9b154f6efd1ad5854cdc56a528da2e02e756e0aa58e"
+  url "https://github.com/mattthias/slurm/archive/upstream/0.4.3-1.tar.gz"
+  sha256 "b960c0d215927be1d02c176e1b189321856030226c91f840284886b727d3a3ac"
 
   bottle do
     cellar :any_skip_relocation
