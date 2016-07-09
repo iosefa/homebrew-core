d41cc897b1025b765f3a1716185d5a51ef322513
ilovezfs <ilovezfs@icloud.com>
2016-07-08 22:22:01 -0700

    finatra 2.2.0

--- Formula/finatra.rb
+++ Formula/finatra.rb
@@ -1,8 +1,8 @@
 class Finatra < Formula
   desc "Scala web framework inspired by Sinatra"
   homepage "http://finatra.info/"
-  url "https://github.com/twitter/finatra/archive/1.5.3.tar.gz"
-  sha256 "aa4fab5ccdc012da9edf4650addf54b6ba64eb7e6a5e88d8c76e68e4d89216de"
+  url "https://github.com/twitter/finatra/archive/finatra-2.2.0.tar.gz"
+  sha256 "a3fa441ac65303f0ac05a821c2644b9bacbf7cc916fd0cee4bbb6273296693e8"
 
   bottle :unneeded
 
