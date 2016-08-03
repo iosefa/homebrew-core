ea02a925cc1651bff40da2c57d9f7fb2d2afb232
ilovezfs <ilovezfs@icloud.com>
2016-08-02 18:31:32 -0700

    dspdfviewer: use bottle :disabled

--- Formula/dspdfviewer.rb
+++ Formula/dspdfviewer.rb
@@ -5,12 +5,7 @@ class Dspdfviewer < Formula
   sha256 "19986053ac4a60e086a9edd78281f1e422a64efef29e3c6604386419e9420686"
   head "https://github.com/dannyedel/dspdfviewer.git"
 
-  bottle do
-    cellar :any
-    sha256 "5f23135aa30bf6fae45d453d892361daef51e19ffe510f5a9e79b5ed7393d0e3" => :el_capitan
-    sha256 "69bf9e53d7b8fd5414f770baea119c7f22d1a05f752c7915cbe1f0cdc201a004" => :yosemite
-    sha256 "d2f2ad9306d3417ab848ed98ca47481242a4a4064f8a8a1e1e6cc41aa286d4aa" => :mavericks
-  end
+  bottle :disable, "Poppler built with Qt 5 causes CI conflicts"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
