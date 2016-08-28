821c849cb23c888bb32a40c421a44ccaeda442c2
ilovezfs <ilovezfs@icloud.com>
2016-08-28 16:29:35 -0700

    jing 20160828-alpha1

--- Formula/jing.rb
+++ Formula/jing.rb
@@ -1,13 +1,21 @@
 class Jing < Formula
   desc "RELAX NG validator"
-  homepage "https://code.google.com/p/jing-trang/"
-  url "https://jing-trang.googlecode.com/files/jing-20091111.zip"
-  sha256 "57690280aa6b5521b570aaa5fe77e1b48d84b2a1b0a24da62f9b982c4416908c"
+  homepage "https://github.com/relaxng/jing-trang"
+  url "https://github.com/relaxng/jing-trang.git",
+      :revision => "1e74846999bbd14ce5248acbd2be9f1e624a9846"
+  version "20160828-alpha1"
+  head "https://github.com/relaxng/jing-trang.git"
 
   bottle :unneeded
+  depends_on :java
 
   def install
+    system "./ant"
     libexec.install Dir["*"]
-    bin.write_jar_script libexec/"bin/jing.jar", "jing"
+    bin.write_jar_script libexec/"build/jing.jar", "jing"
+  end
+
+  test do
+    assert_match "catalogFile", shell_output("#{bin}/jing", 2)
   end
 end
