c88052f75f3bd69afbeea5492aee8250cdc071a0
ilovezfs <ilovezfs@icloud.com>
2016-10-15 00:18:58 -0700

    hyperscan: revision for boost

--- Formula/hyperscan.rb
+++ Formula/hyperscan.rb
@@ -3,6 +3,7 @@ class Hyperscan < Formula
   homepage "https://01.org/hyperscan"
   url "https://github.com/01org/hyperscan/archive/v4.3.1.tar.gz"
   sha256 "a7bce1287c06d53d1fb34266d024331a92ee24cbb2a7a75061b4ae50a30bae97"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
