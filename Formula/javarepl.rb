f5ccacc31bde22934f10b2e58a875625873caa16
ilovezfs <ilovezfs@icloud.com>
2016-11-30 20:41:16 -0800

    javarepl 362

--- Formula/javarepl.rb
+++ Formula/javarepl.rb
@@ -1,13 +1,13 @@
 class Javarepl < Formula
   desc "Read Eval Print Loop (REPL) for Java"
   homepage "https://github.com/albertlatacz/java-repl"
-  url "https://s3.amazonaws.com/albertlatacz.published/repo/javarepl/javarepl/350/javarepl-350.jar"
-  sha256 "82fa4144c8863b5dd441fc6a5dd7d8f6191a7de94406e1de5c6d1a08f992140a"
+  url "https://github.com/albertlatacz/java-repl/releases/download/362/java-repl-362.jar"
+  sha256 "069ab1a3d54828a397b28869d695dcbfc27081043aaf89f0125e6094851f8931"
 
   bottle :unneeded
 
   def install
-    libexec.install "javarepl-#{version}.jar"
+    libexec.install "java-repl-#{version}.jar"
     bin.write_jar_script libexec/"javarepl-#{version}.jar", "javarepl"
   end
 
