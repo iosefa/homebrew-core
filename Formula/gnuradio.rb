3b4a0b68a7e8a299e07326fde639224dda4aad0d
ilovezfs <ilovezfs@icloud.com>
2016-12-05 04:37:12 -0800

    gnuradio 4.0-alpha1
    restore the GUI
    use qt5 and python3

--- Formula/gnuradio.rb
+++ Formula/gnuradio.rb
@@ -1,9 +1,10 @@
 class Gnuradio < Formula
   desc "SDK providing the signal processing runtime and processing blocks"
   homepage "https://gnuradio.squarespace.com/"
-  url "https://gnuradio.org/releases/gnuradio/gnuradio-3.7.9.1.tar.gz"
-  sha256 "9c06f0f1ec14113203e0486fd526dd46ecef216dfe42f12d78d9b781b1ef967e"
-  revision 1
+  url "https://github.com/gnuradio/gnuradio.git",
+      :revision => "dd3e3c8198515e10cc91145a16b08d0f37ee1bd1"
+  version "4.0-alpha1"
+  head "https://github.com/gnuradio/gnuradio.git", :branch => "next"
 
   bottle do
     rebuild 1
@@ -12,20 +13,43 @@ class Gnuradio < Formula
     sha256 "41eb9fdae72761b7a83f284f1e7613da9e3ce916e0f98f20ad3aafe417be5a4e" => :yosemite
   end
 
+  # These python extension modules were linked directly to a Python
+  # framework binary.
+  # Replace -lpython with -undefined dynamic_lookup in linker flags.
+  # https://github.com/gnuradio/gnuradio/pull/604
+  patch do
+    url "https://github.com/gnuradio/gnuradio/pull/604.patch"
+    sha256 "9e1c612f0f4063d387d85517cc420f050f49c7903d36fab45b72e8d828549e3c"
+  end
+
+  option "with-documentation", "Build with documentation"
   option :universal
 
   depends_on "pkg-config" => :build
 
-  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on :python3 => :recommended
   depends_on "boost"
   depends_on "cppunit"
   depends_on "fftw"
   depends_on "gsl"
   depends_on "zeromq"
 
-  # For documentation
-  depends_on "doxygen" => :build
-  depends_on "sphinx-doc" => :build
+  if build.with? "python3"
+    depends_on "swig" => :build
+    depends_on "pygtk"
+    depends_on "wxpython"
+    depends_on "qt5"
+    depends_on "qwt"
+    depends_on "pyqt5"
+    depends_on "orc"
+    depends_on "log4cpp"
+    depends_on "thrift"
+  end
+
+  if build.with? "documentation"
+    depends_on "doxygen" => :build
+    depends_on "sphinx-doc" => :build
+  end
 
   depends_on "uhd" => :recommended
   depends_on "sdl" => :recommended
@@ -39,25 +63,25 @@ class Gnuradio < Formula
   end
 
   resource "numpy" do
-    url "https://pypi.python.org/packages/source/n/numpy/numpy-1.10.1.tar.gz"
-    sha256 "8b9f453f29ce96a14e625100d3dcf8926301d36c5f622623bf8820e748510858"
+    url "https://files.pythonhosted.org/packages/16/f5/b432f028134dd30cfbf6f21b8264a9938e5e0f75204e72453af08d67eb0b/numpy-1.11.2.tar.gz"
+    sha256 "04db2fbd64e2e7c68e740b14402b25af51418fc43a59d9e54172b38b906b0f69"
   end
 
   # cheetah starts here
   resource "Markdown" do
-    url "https://pypi.python.org/packages/source/M/Markdown/Markdown-2.4.tar.gz"
-    sha256 "b8370fce4fbcd6b68b6b36c0fb0f4ec24d6ba37ea22988740f4701536611f1ae"
+    url "https://files.pythonhosted.org/packages/d4/32/642bd580c577af37b00a1eb59b0eaa996f2d11dfe394f3dd0c7a8a2de81a/Markdown-2.6.7.tar.gz"
+    sha256 "daebf24846efa7ff269cfde8c41a48bb2303920c7b2c7c5e04fa82e6282d05c0"
   end
 
   resource "Cheetah" do
-    url "https://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz"
+    url "https://files.pythonhosted.org/packages/cd/b0/c2d700252fc251e91c08639ff41a8a5203b627f4e0a2ae18a6b662ab32ea/Cheetah-2.4.4.tar.gz"
     sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
   end
   # cheetah ends here
 
   resource "lxml" do
-    url "https://pypi.python.org/packages/source/l/lxml/lxml-2.0.tar.gz"
-    sha256 "062e6dbebcbe738eaa6e6298fe38b1ddf355dbe67a9f76c67a79fcef67468c5b"
+    url "https://files.pythonhosted.org/packages/4f/3f/cf6daac551fc36cddafa1a71ed48ea5fd642e5feabd3a0d83b8c3dfd0cb4/lxml-3.6.4.tar.gz"
+    sha256 "61d5d3e00b5821e6cda099b3b4ccfea4527bf7c595e0fb3a7a760490cedd6172"
   end
 
   resource "cppzmq" do
@@ -65,9 +89,15 @@ class Gnuradio < Formula
     sha256 "f042d4d66e2a58bd951a3eaf108303e862fad2975693bebf493931df9cd251a5"
   end
 
+  resource "Mako" do
+    url "https://files.pythonhosted.org/packages/7a/ae/925434246ee90b42e8ef57d3b30a0ab7caf9a2de3e449b876c56dcb48155/Mako-1.0.4.tar.gz"
+    sha256 "fed99dbe4d0ddb27a33ee4910d8708aca9ef1fe854e668387a9ab9a90cbf9059"
+  end
+
   def install
     ENV["CHEETAH_INSTALL_WITHOUT_SETUPTOOLS"] = "1"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    ENV.prepend_path "PYTHONPATH", Formula["pyqt5"].opt_lib/"python3.5/site-packages"
 
     resource("cmake").stage do
       args = %W[
@@ -95,7 +125,7 @@ class Gnuradio < Formula
 
     ENV.prepend_path "PATH", buildpath/"cmake/bin"
 
-    res = %w[Markdown Cheetah lxml numpy]
+    res = %w[Markdown Cheetah lxml numpy Mako]
     res.each do |r|
       resource(r).stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
@@ -119,6 +149,7 @@ class Gnuradio < Formula
     enabled_components << "gr-wavelet"
     enabled_components << "gr-video-sdl" if build.with? "sdl"
     enabled_components << "gr-uhd" if build.with? "uhd"
+    enabled_components += %w[python gr-ctrlport grc gr-utils gr-qtgui gr-wxgui] if build.with? "python3"
     enabled_components += %w[doxygen sphinx] if build.with? "documentation"
 
     enabled_components.each do |c|
