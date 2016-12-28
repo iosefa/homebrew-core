28d658e504fac6e2eb6e8fd1ded23faafc7a970d
ilovezfs <ilovezfs@icloud.com>
2016-12-28 01:56:16 -0800

    redsocks 0.5

--- Formula/redsocks.rb
+++ Formula/redsocks.rb
@@ -1,8 +1,8 @@
 class Redsocks < Formula
   desc "Transparent socks redirector"
   homepage "http://darkk.net.ru/redsocks"
-  url "https://github.com/darkk/redsocks/archive/release-0.4.tar.gz"
-  sha256 "618cf9e8cd98082db31f4fde6450eace656fba8cd6b87aa4565512640d341045"
+  url "https://github.com/darkk/redsocks/archive/release-0.5.tar.gz"
+  sha256 "bbeb531d7f1986d7102f1bd6733dacce41d3f3ba7d604f1aab61c63e0ba2ee62"
 
   bottle do
     cellar :any
