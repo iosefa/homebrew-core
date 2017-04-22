dbd81e8d4bcfe06e6f718c51980fd4a50a19b82c
ilovezfs <ilovezfs@icloud.com>
2017-04-22 03:24:40 -0700

    iproute2mac 1.2.0

--- Formula/iproute2mac.rb
+++ Formula/iproute2mac.rb
@@ -1,8 +1,8 @@
 class Iproute2mac < Formula
   desc "CLI wrapper for basic network utilities on macOS - ip command"
   homepage "https://github.com/brona/iproute2mac"
-  url "https://github.com/brona/iproute2mac/archive/v1.1.2.tar.gz"
-  sha256 "e228484bc54e56fc5932016f3e77751a1db155d01132553d1dad27b0fcc00b18"
+  url "https://github.com/brona/iproute2mac/archive/v1.2.0.tar.gz"
+  sha256 "d04f08ee2070ed92c3b1416f0bf02b0bd65ea16f7c380113a0ad7769882e0ef4"
 
   bottle :unneeded
 
