355420bf32627969dc107f83d8a9b1981f2707e9
ilovezfs <ilovezfs@icloud.com>
2017-06-28 09:45:57 -0700

    gcviewer 1.35

--- Formula/gcviewer.rb
+++ Formula/gcviewer.rb
@@ -1,8 +1,8 @@
 class Gcviewer < Formula
   desc "Java garbage collection visualization tool"
   homepage "https://github.com/chewiebug/GCViewer"
-  url "https://github.com/chewiebug/GCViewer/archive/1.34.1.tar.gz"
-  sha256 "e0e97a94c80be8323772dc8953d463fe8eaf60c3a1c5f212d0b575a3b5c640ba"
+  url "https://github.com/chewiebug/GCViewer/archive/1.35.tar.gz"
+  sha256 "26be619d0112c2985dd2a1909337419317d59905c3decea00588f008fa6eaa7f"
 
   bottle do
     cellar :any_skip_relocation
