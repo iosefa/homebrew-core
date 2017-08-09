895e2583989e4dc48e5fde816c80d72e38822ee8
ilovezfs <ilovezfs@icloud.com>
2017-08-09 03:29:22 -0700

    Update arx.rb

--- Formula/arx.rb
+++ Formula/arx.rb
@@ -7,7 +7,6 @@ class Arx < Formula
   homepage "https://github.com/solidsnack/arx"
   url "https://github.com/solidsnack/arx/archive/0.2.2.tar.gz"
   sha256 "47e7a61a009d43c40ac0ce9c71917b0f967ef880c99d4602c7314b51c270fd0f"
-  revision 1
 
   bottle do
     cellar :any_skip_relocation
