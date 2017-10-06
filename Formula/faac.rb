e3b79779049bebcde54e76fb0b1a1a4c638cf116
ilovezfs <ilovezfs@icloud.com>
2017-10-06 10:31:54 -0700

    faac 1.29.7.8

--- Formula/faac.rb
+++ Formula/faac.rb
@@ -1,8 +1,8 @@
 class Faac < Formula
   desc "ISO AAC audio encoder"
   homepage "http://www.audiocoding.com/faac.html"
-  url "https://downloads.sourceforge.net/project/faac/faac-src/faac-1.29/faac-1.29.7.7.tar.gz"
-  sha256 "b898fcf55e7b52f964ee62d077f56fe9b3b35650e228f006fbef4ce903b4d731"
+  url "https://downloads.sourceforge.net/project/faac/faac-src/faac-1.29/faac-1.29.7.8.tar.gz"
+  sha256 "316fff598764b93485dc5fb41379768b8c69348bbbb87e0e30ce28b1da487251"
 
   bottle do
     cellar :any
