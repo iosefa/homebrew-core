ecc93be37d584bff095161b57b09d4e2695c0724
ilovezfs <ilovezfs@icloud.com>
2017-11-11 23:08:44 -0800

    nativefier 7.5.0

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Nativefier < Formula
   desc "Wrap web apps natively"
   homepage "https://github.com/jiahaog/nativefier"
-  url "https://registry.npmjs.org/nativefier/-/nativefier-7.4.1.tgz"
-  sha256 "c6498a57a02a31416c8452fb86dbc8dcfd927a069646d53c4aeb67cf848f87ba"
+  url "https://registry.npmjs.org/nativefier/-/nativefier-7.5.0.tgz"
+  sha256 "3b5252622434e9112c6c040ec32ba3f5a2a45ebc4943885ffe7493a5078d7237"
 
   bottle do
     cellar :any_skip_relocation
