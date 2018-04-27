2efa36e807a603897c25f986d405389d31cce28e
ilovezfs <ilovezfs@icloud.com>
2018-04-27 06:26:29 +0000

    Update tee-clc.rb

--- Formula/tee-clc.rb
+++ Formula/tee-clc.rb
@@ -1,6 +1,6 @@
 class TeeClc < Formula
   desc "Microsoft Team Explorer Everywhere command-line Client"
-  homepage "https://java.visualstudio.com/Docs/tools/eclipse"
+  homepage "https://github.com/Microsoft/team-explorer-everywhere"
   url "https://github.com/Microsoft/team-explorer-everywhere/releases/download/14.134.0/TEE-CLC-14.134.0.zip"
   sha256 "af4b7123a09475ff03a3f5662df3de614df2f4acc33df16cdab307b5fb6d7dc7"
 
