1af0a754a42689e6f84d8ddd4c6f7247a5ec8045
ilovezfs <ilovezfs@icloud.com>
2018-06-28 23:53:13 -0700

    libnfs 3.0.0

--- Formula/libnfs.rb
+++ Formula/libnfs.rb
@@ -1,8 +1,8 @@
 class Libnfs < Formula
   desc "C client library for NFS"
   homepage "https://github.com/sahlberg/libnfs"
-  url "https://github.com/sahlberg/libnfs/archive/libnfs-2.0.0.tar.gz"
-  sha256 "7ea6cd8fa6c461d01091e584d424d28e137d23ff4b65b95d01a3fd0ef95d120e"
+  url "https://github.com/sahlberg/libnfs/archive/libnfs-3.0.0.tar.gz"
+  sha256 "445d92c5fc55e4a5b115e358e60486cf8f87ee50e0103d46a02e7fb4618566a5"
 
   bottle do
     cellar :any
