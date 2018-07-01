65936670a2cb0c543f304847c38d56fef98e0111
ilovezfs <ilovezfs@icloud.com>
2018-06-30 21:37:30 -0700

    advancemame 3.8

--- Formula/advancemame.rb
+++ Formula/advancemame.rb
@@ -1,8 +1,8 @@
 class Advancemame < Formula
   desc "MAME with advanced video support"
   homepage "https://www.advancemame.it/"
-  url "https://github.com/amadvance/advancemame/releases/download/v3.7/advancemame-3.7.tar.gz"
-  sha256 "36c88305dc485e85ff86854b3d8bb75c4c81fa7356f6dbfcbfd6a5e192199b2c"
+  url "https://github.com/amadvance/advancemame/releases/download/v3.8/advancemame-3.8.tar.gz"
+  sha256 "6170d5e1d97dd466764cb7ced5cc3497dfa1ad5578fa5efebe4263b417692491"
 
   bottle do
     sha256 "cebd9f5bdee213f0994527f63a3c8ad6d78cb43e377bdd19978fa2d9e03acd15" => :high_sierra
